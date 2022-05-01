2d8941d50e29a8e4ecf5f095c9277fb4c0022e3c
majajonez <majajonez@users.noreply.github.com>
2022-05-01 13:19:19 +0200

    poprawki do zapisywania i odczytywania kontaktów

--- aplikacja.py
+++ aplikacja.py
@@ -1,70 +1,96 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk
 from tkinter.messagebox import showinfo
+import json
+
 
 ekran = tk.Tk()
 ekran.title("książka telefoniczna")
-ekran.geometry("800x600")
+# ekran.geometry("600x800")
+frame = ttk.Frame(ekran, padding="5")
+frame.grid(column=0, row=0, sticky="wnes", columnspan=3)
 
 
-def okno_kontaktu():
-    window = tk.Toplevel()
-    elementy_kontaktu = ["IMIE", "NAZWISKO", "NR TELEFONU"]
-    for i in elementy_kontaktu:
-        label = tk.Label(window, text=i)
-        label.pack(fill='x', padx=100, pady=5)
+class Kontakt:
+    def __init__(self, imie, nazwisko, nr_tel):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.nr_tel = nr_tel
 
-    button_close = tk.Button(window, text="Close", command=window.destroy)
-    button_close.pack(fill='x')
+    def __repr__(self):
+        return f' {self.imie} {self.nazwisko} {self.nr_tel}'
+
+    def ustaw_imie(self, imie):
+        self.imie = imie
+
+
+f = open('zap_k.json', "r")
+kontakty = json.loads(f.read())
+
+
+def from_json(json_kontakt):
+    return Kontakt(json_kontakt["imie"], json_kontakt["nazwisko"], json_kontakt["nr_tel"])
+lista_kontaktow = []
+
+for json_kontakt in kontakty:
+    kontakt = from_json(json_kontakt)
+    lista_kontaktow.append(kontakt)
+
+
+kontakty_var = tk.StringVar(value=lista_kontaktow)
+widok_kontaktow = Listbox(frame, listvariable=kontakty_var, selectmode='extended')
+widok_kontaktow.grid(row=0, column=0, pady="5 20", columnspan=2, sticky="nwes")
+scrollbar = tk.Scrollbar(ekran, orient='vertical', command=widok_kontaktow.yview)
+scrollbar.grid(row=0, column=3, sticky='nse')
+widok_kontaktow.config(yscrollcommand=scrollbar.set)
 
 def tworzenie_nowego_kontaktu():
     window = tk.Toplevel()
-    input_kontakt(window, "podaj imie", 0, lambda z: imiona.append(z))
-    input_kontakt(window, "podaj nazwisko", 1, lambda z: nazwiska.append(z))
-    input_kontakt(window, "podaj nr tel", 2, lambda z: nr_tel.append(z))
-    button_close = tk.Button(window, text="Close", command=window.destroy)
-    button_close.grid(row=3, column=0, sticky='we')
+    okno_imie = input_kontakt(window, "podaj imie", 0)
+    okno_nazwisko = input_kontakt(window, "podaj nazwisko", 1)
+    okno_nr = input_kontakt(window, "podaj nr tel", 2)
 
-def input_kontakt(window, tekst, row, x):
+
+
+    def action():
+        kontakt = Kontakt(okno_imie.get(), okno_nazwisko.get(), okno_nr.get())
+        plik = "zap_k.json"
+        lista_kontaktow.append(kontakt)
+        lista_map = []
+        for k in lista_kontaktow:
+            k_json = k.__dict__
+            lista_map.append(k_json)
+
+
+        with open(plik, "w", encoding="utf-8") as uchwyt_do_pliku:
+            caly_json = json.dumps(lista_map)
+            uchwyt_do_pliku.write(caly_json)
+        kontakty_var.set(lista_kontaktow)
+        print(kontakty)
+        window.destroy()
+
+    button_zapisz = tk.Button(window, text="Zapisz", command=action)
+    button_zapisz.grid(row=3, column=0, sticky='we')
+
+
+def input_kontakt(window, tekst, row):
     label1 = Label(window, text=tekst)
     label1.grid(row=row, column=0)
     okno = Entry(window)
-
-    def wydrukuj(self):
-        zawartosc = okno.get()
-        x(zawartosc)
-
-    okno.grid(row=row, column=1)
-    okno.bind('<Return>', wydrukuj)
-    okno.bind('<Tab>', wydrukuj)
-
-
-imiona = []
-nazwiska = []
-nr_tel = []
-
-# lista_kontaktow = ["Maja", "Kasper"]
-# a = 20
-# for i in lista_kontaktow:
-#     kontakt = tk.Button(ekran, text=i, command=okno_kontaktu, height=2, width=20)
-#     kontakt.place(x=20, y=a)
-#     a += 50
-
-kontakty = ["Maja", "Kasper", "Lucyfer", "Markiz", "Bonifacy"]
-kontakty_var = tk.StringVar(value=kontakty)
-lista_kontaktow = Listbox(ekran, listvariable=kontakty_var, height=30, width=132, selectmode='extended')
-lista_kontaktow.grid(column=0, row=0, sticky='nwes')
+    okno.grid(column=2, row=row, sticky=(N, W, E, S))
+    okno.focus_set()
+    return okno
 
 
 
-# nowy_kontakt = tk.Button(ekran, text="dodaj nowy kontakt", command=tworzenie_nowego_kontaktu, height=3, width=20, bg='#567', fg='White')
-# nowy_kontakt.place(x=20, y=500)
+nowy_kontakt = tk.Button(frame, text="dodaj nowy kontakt", command=tworzenie_nowego_kontaktu, bg='#567', fg='White')
+nowy_kontakt.grid(row=2, column=0)
 
-
-
+edytuj_kontakt = tk.Button(frame, text="edytuj kontakt", command=tworzenie_nowego_kontaktu, bg='#567', fg='White')
+edytuj_kontakt.grid(row=2, column=1)
 
 
 ekran.mainloop()
-
 # lista w listbox
 # dodaj kontakt, wpisze sie do tej listy
--- maly projekt.py
+++ maly projekt.py
@@ -1,74 +1,42 @@
 from tkinter import *
-import tkinter as tk
-from tkinter.messagebox import showinfo
-from tkinter import ttk
 
-ekran = tk.Tk()
-ekran.title("książka telefoniczna")
-ekran.geometry("800x600")
+ws = Tk()
+Frm = Frame(ws)
+Label(Frm, text='Enter Word to Find:').pack(side=LEFT)
+modify = Entry(Frm)
 
-class Kontakt:
-    def __init__(self, imie, nazwisko, nr_tel):
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.nr_tel = nr_tel
+modify.pack(side=LEFT, fill=BOTH, expand=1)
 
-    def __repr__(self):
-        return f' {self.imie} {self.nazwisko} {self.nr_tel}'
+modify.focus_set()
 
-    def ustaw_imie(self, imie):
-        self.imie = imie
+buttn = Button(Frm, text='Find')
+buttn.pack(side=RIGHT)
+Frm.pack(side=TOP)
 
-# def tworzenie_nowego_kontaktu():
-#     window = tk.Toplevel()
-#     label1 = Label(window, text="Podaj imie :")
-#     label1.grid(row=0, column=0)
-#     okno = Entry(window)
-#
-#     def zapisz(self):
-#         zawartosc = okno.get()
-#         kontakty.append(zawartosc)
-#         print(kontakty)
-#         kontakty_var.set(kontakty)
-#
-#     okno.grid(row=0, column=1)
-#     okno.bind('<Return>', zapisz)
-# mainframe = ttk.Frame(ekran, padding="3 3 12 12")
-# mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-# ekran.columnconfigure(0, weight=1)
-# ekran.rowconfigure(0, weight=1)
+txt = Text(ws)
 
-kontakty = []
-kontakty_var = tk.StringVar(value=kontakty)
-lista_kontaktow = Listbox(ekran, listvariable=kontakty_var, height=30, width=132, selectmode='extended')
-lista_kontaktow.grid(column=0, row=0, sticky='nwes')
-
-def tworzenie_nowego_kontaktu():
-    window = tk.Toplevel()
-    okno_imie = input_kontakt(window, "podaj imie", 0)
-    okno_nazwisko = input_kontakt(window, "podaj nazwisko", 1)
-    okno_nr = input_kontakt(window, "podaj nr tel", 2)
+txt.insert('1.0',
+           '''English comprehension passage is the good test of your language skills. The questions in the reading comprehension exercise are mostly inference based...''')
+txt.pack(side=BOTTOM)
 
 
-    def action():
-        kontakt = Kontakt(okno_imie.get(), okno_nazwisko.get(), okno_nr.get())
-        kontakty.append(kontakt)
-        kontakty_var.set(kontakty)
-        print(kontakty)
-        window.destroy()
+def find():
+    txt.tag_remove('found', '1.0', END)
+    ser = modify.get()
+    if ser:
+        idx = '1.0'
+        while 1:
+            idx = txt.search(ser, idx, nocase=1,
+                             stopindex=END)
+            if not idx: break
+            lastidx = '%s+%dc' % (idx, len(ser))
 
-    button_close = tk.Button(window, text="Close", command=action)
-    button_close.grid(row=3, column=0, sticky='we')
-
-def input_kontakt(window, tekst, row):
-    label1 = Label(window, text=tekst)
-    label1.grid(row=row, column=0)
-    okno = Entry(window)
-    okno.grid(column=2, row=row, sticky=(N, W, E, S))
-    return okno
-
+            txt.tag_add('found', idx, lastidx)
+            idx = lastidx
+        txt.tag_config('found', foreground='blue')
+    modify.focus_set()
 
 
-nowy_kontakt = tk.Button(ekran, text="dodaj nowy kontakt", command=tworzenie_nowego_kontaktu, height=3, width=20, bg='#567', fg='White')
-nowy_kontakt.grid(column=0, row=3, sticky=(N, W, E, S))
-ekran.mainloop()
+buttn.config(command=find)
+
+ws.mainloop()
